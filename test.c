2d41146b146bdd775e2af9cf953118c1d9795c98
SchoMartHSLU <martin.schoen@hslu.ch>
2020-09-28 17:25:20 +0200

    Initial commit

---
+++ test.c
@@ -0,0 +1,15 @@
+/* 
+First test program Martin Schoen, 28.9.2020
+*/
+
+int main(void)
+{
+    int readValue = 0;
+    printf("Enter an int: ");
+    scanf("%d", &readValue);
+
+    print("Value Entered: %d \n", readValue);
+
+    return 0;
+       
+}
