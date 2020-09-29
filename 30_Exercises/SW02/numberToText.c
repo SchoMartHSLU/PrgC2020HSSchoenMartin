7b9c93825b1ae6730655bd5f71a5cced992a848f
SchoMartHSLU <martin.schoen@hslu.ch>
2020-09-29 13:35:01 +0200

    Uebung zu SW02

--- 30_Exercises/SW02/numberToText.c
+++ 30_Exercises/SW02/numberToText.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+int readValue = 0;
 
-int main( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
 
+    printf("Enter a number between (including) 0 and 9 to display the number as text. Enter 10 to stop the program: ");
+    scanf("%d", &readValue);
 
+    if (readValue > 10 || readValue < 0)
+    {
+        printf("Invalid -> try again");
+    }
+    else if (readValue == 10)
+    {
+        return 0;    
+    }
+    else
+    {
+        printf("Value inputed: %d", readValue);
+           
+    }
 
+    
 
-	return 0;
+    
 }
