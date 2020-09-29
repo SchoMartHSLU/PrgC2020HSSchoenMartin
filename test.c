5e3a3657bab8d50bd3051bc489916c5ebbc7202a
SchoMartHSLU <martin.schoen@hslu.ch>
2020-09-29 08:13:51 +0200

    Fertigstellung SW01

--- test.c
+++ test.c
@@ -2,14 +2,15 @@
 First test program Martin Schoen, 28.9.2020
 */
 
+#include <stdio.h>
+
 int main(void)
 {
     int readValue = 0;
     printf("Enter an int: ");
     scanf("%d", &readValue);
 
-    print("Value Entered: %d \n", readValue);
+    printf("Value Entered: %d \n", readValue);
 
     return 0;
-       
 }
